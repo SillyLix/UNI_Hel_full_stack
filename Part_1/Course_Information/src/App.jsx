d762d5ad194bad4ed7dd0a7d9ab170d569d8b2d0
Prajwal dhakal <147418042+SillyLix@users.noreply.github.com>
2026-07-19 22:07:42 +0300

    task 1 done

--- Part_1/Course_Information/src/App.jsx
+++ Part_1/Course_Information/src/App.jsx
@@ -1,3 +1,47 @@
+const Header = (prams) => {
+  return (
+    <div>
+      <h1>{prams.courseName}</h1>
+    </div>
+  )
+}
+
+const Content = (prams) => {
+  if (prams.exercise_Array.length != prams.part_array.length) {
+    console.error("part_array and exercise_Array was not the same length when insetting Content")
+  }
+
+  let contents = [];
+
+  for (let index = 0; index < prams.part_array.length; index++) {
+    contents.push(
+      <p key={index}>
+      {prams.part_array[index]} {prams.exercise_Array[index]}
+      </p>
+
+    )
+  }
+      
+  return (
+    <div>
+      {contents}
+    </div>
+  )
+}
+
+const Total = (prams) => {
+  let totalNum = 0;
+  for (let index = 0; index < prams.exercise_Array.length; index++) {
+    totalNum += prams.exercise_Array[index];
+  }
+
+  return (
+    <p>
+        Number of exercises {totalNum}
+    </p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development';
   const part1 = 'Fundamentals of React';
@@ -6,22 +50,14 @@ const App = () => {
   const exercises2 = 7;
   const part3 = 'State of a component';
   const exercises3 = 14;
-
+  const exerciseArray = [exercises1, exercises2, exercises3];
+  const partArray = [part1, part2, part3];
   return (
+    
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>
-        Number of exercises {exercises1+exercises2+exercises3}
-      </p>
+      <Header courseName={course} />
+      <Content part_array={partArray} exercise_Array={exerciseArray} />
+      <Total  exercise_Array={exerciseArray}/>
     </div>
   )
 }

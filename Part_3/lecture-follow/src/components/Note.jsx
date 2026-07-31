f8de1a37d3d540a982dd5d420def73c4a0f9b350
Prajwal dhakal <147418042+SillyLix@users.noreply.github.com>
2026-07-31 15:04:49 +0300

    sending lecture to main

---
+++ Part_3/lecture-follow/src/components/Note.jsx
@@ -0,0 +1,18 @@
+import './button.css';
+
+const Note = ({ note, toggleImportance }) => {
+	const label = note.important ? `make not important` : `make important`;
+	return (
+		<li className="note">
+			{note.content + ' '}
+			<button
+				className="impButton"
+				onClick={toggleImportance}
+			>
+				{label}
+			</button>
+		</li>
+	);
+};
+
+export default Note;
